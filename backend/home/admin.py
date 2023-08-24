3d66bde6047dad9c450d6850a42d31a694666523
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-24 08:27:19 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Sxwe1
+admin.site.register(Sxwe1)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Sxwe1(models.Model):
+    'Generated Model'
+    hfhgj = models.BigIntegerField()
+    tryruu = models.BigIntegerField()
